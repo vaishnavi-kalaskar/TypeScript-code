0c3a07b9eddb0345062a07783e6c65e51c5088dc
vaishnavi-kalaskar <vaishnavikalskar02@gmail.com>
2024-09-11 13:27:54 +0530

    Add files via upload

---
+++ index-9-sep.js
@@ -0,0 +1,4 @@
+var a = 35;
+console.log(a);
+var b = "hello";
+var arr = [3, 4, 5];
---
+++ index-9-sep.ts
@@ -0,0 +1,8 @@
+var a = 35;
+console.log(a);
+
+var b = "hello";
+console.log(b);
+
+let arr = [3, 4, 5];
+console.log(arr);
